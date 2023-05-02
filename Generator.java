c46d7ab41f15fb71763e400113189ddf82d28b14
CherylLim <85498185+dracolim@users.noreply.github.com>
2023-05-02 13:39:41 +0800

    write into output file

--- Generator.java
+++ Generator.java
@@ -7,19 +7,41 @@ public class Generator {
             File file = new File("test.txt");
             Scanner filein = new Scanner(file);
 
+            PrintStream writer = new PrintStream(new FileOutputStream("output.txt") , false);
             while (filein.hasNext()){
                 String currentLine = filein.nextLine();
-                String[] temp_list = currentLine.split(" ");
+                String edited = currentLine.replaceAll("\\s+", " ");
+                String[] temp_list = edited.split(" ");
 
-                System.out.println(Array.toString(temp_list));
-                // for (String item : temp_list) {
-                //     System.out.println(item);
-                // }
+                // convert array to arraylist
+                ArrayList<String> temp_ArrayList = new ArrayList<String>();
+                for (String item : temp_list) {
+                    temp_ArrayList.add(item);
+                }
+
+                for (int i = 0 ; i < temp_ArrayList.size() ; i += 1){
+                    String item = temp_list[i];
+                    if (item.contains("MC")){
+                        String new_item = temp_ArrayList.get(i) + " " + temp_ArrayList.get(i + 1);
+                        temp_ArrayList.remove(i + 1);
+                        temp_ArrayList.set(i , new_item);
+                    }
+                }
+                System.out.println(temp_ArrayList);
+
+                String result = "";
+                for (String string : temp_ArrayList) {
+                    result += string + " ";
+                }
+
+                writer.println(result);
+
             }
+            filein.close();
+            writer.close();
         }
         catch(FileNotFoundException e){
-
+            System.out.println(e.getMessage());
         }
-
     }
 }
